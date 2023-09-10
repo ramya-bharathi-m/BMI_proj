2f9c748dec52b93592cc616d70aebdf98780596c
Ramya Bharathi M <ramyabharathim22@gmail.com>
2023-09-10 20:20:31 +0530

    app.js changes

--- app.js
+++ app.js
@@ -29,7 +29,20 @@ function bmi(){
     let weight = document.getElementById('weight')
 
     let result = weight.value / (height.value * height.value)
-    if(result > 40){
-        document.getElementById("result").innerHTML = "Obese";
+    if(result < 18.5){
+        document.getElementById('result').innerHTML = 'Way Too Thin !! Eat UP'
+    }
+    else if(result>18.5 && result <24.9){
+        document.getElementById('result').innerHTML = 'Your BMI is Perfect'
+    }
+    else if(result>25 && result <29.9){
+        document.getElementById('result').innerHTML = 'You are a little above the Perfect BMI Line !! Consider Dieting'
+    }
+    else if(result>30 && result <35){
+        document.getElementById('result').innerHTML = 'obesity 😔'
+    }
+    else{
+        document.getElementById('result').innerHTML = 'Way too Above the Line'
+
     }
 }
